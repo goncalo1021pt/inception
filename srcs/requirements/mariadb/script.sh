6497f29d2e5780c12cbe4595b00e971998766a40
goncalo1021pt <goncalo.pereira.1021@gmail.com>
2024-10-03 18:04:11 +0100

    {}

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,17 @@
+select;
+from ;
+where ;
+left join ;
+right join ;
+inner join ;
+group by ;
+order by ;
+on ;
+truncate ; // delete all data in table
+delete ; // delete data in table
+insert into ; // insert data in table
+update ; // update data in table
+
+
+//nginx
+nginx - t // test configuration
