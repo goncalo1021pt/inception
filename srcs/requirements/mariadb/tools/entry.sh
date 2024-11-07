de118fa8b2eebe702112c692248391326ebc3c2c
goncalo1021pt <goncalo.pereira.1201@gmail.com>
2024-11-07 12:40:59 +0000

    backup

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -14,7 +14,8 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 
 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $SQL_DB;"
 	mysql -u root -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASS';"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON $SQL_DB.* TO '$SQL_USER'@'%';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $SQL_DB.* TO '$SQL_USER'@'%' WITH GRANT OPTION;"
+	ALTER USER 'root@localhost' IDENTIFIED BY $SQL_ROOT_PASS;
 	mysql -u root -e "FLUSH PRIVILEGES;"
 
 fi
--- srcs/requirements/wordpress/tools/entry.sh
+++ srcs/requirements/wordpress/tools/entry.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Set up WordPress using WP-CLI
-cd /var/www/html
 
-# Download WordPress
-wp core download --allow-root
+if [ ! -f /var/www/html/wp-config.php ]; then
+    # Download WordPress
+    wp core download --allow-root
 
-# Create wp-config
-wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --allow-root
+    # Create wp-config
+    wp config create --dbname="$SQL_DB" --dbuser="$SQL_USER" --dbpass="$SQL_PASS" --dbhost="$DB_HOST" --allow-root
 
-# Install WordPress
-wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+    # Install WordPress
+    wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_password="$WP_PASS" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 
+fi
 # Start PHP-FPM
 php-fpm
